88e88ea269c2f02071168d78e580130f58a26af9
alexcsych <alexsejsi4@gmail.com>
2023-07-04 14:43:58 +0300

    Fix imageUploader

--- client/src/components/UpdateUserInfoForm/UpdateUserInfoForm.jsx
+++ client/src/components/UpdateUserInfoForm/UpdateUserInfoForm.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Formik } from 'formik';
 import { connect } from 'react-redux';
 import { clearUserError } from '../../store/slices/userSlice';
 import styles from './UpdateUserInfoForm.module.sass';
-import ImageUpload from '../InputComponents/ImageUpload/ImageUpload';
 import FormInput from '../FormInput/FormInput';
 import Schems from '../../utils/validators/validationSchems';
 import Error from '../Error/Error';
 
 const UpdateUserInfoForm = props => {
+  const [imageName, setImageName] = useState('anon.png');
   const { onSubmit, submitting, error, clearUserError } = props;
   return (
     <Formik
@@ -16,68 +16,79 @@ const UpdateUserInfoForm = props => {
       initialValues={props.initialValues}
       validationSchema={Schems.UpdateUserSchema}
     >
-      <Form className={styles.updateContainer}>
-        {error && (
-          <Error
-            data={error.data}
-            status={error.status}
-            clearError={clearUserError}
-          />
-        )}
-        <div className={styles.container}>
-          <span className={styles.label}>First Name</span>
-          <FormInput
-            name='firstName'
-            type='text'
-            label='First Name'
-            classes={{
-              container: styles.inputContainer,
-              input: styles.input,
-              warning: styles.error,
-              notValid: styles.notValid,
-            }}
-          />
-        </div>
-        <div className={styles.container}>
-          <span className={styles.label}>Last Name</span>
-          <FormInput
-            name='lastName'
-            type='text'
-            label='LastName'
-            classes={{
-              container: styles.inputContainer,
-              input: styles.input,
-              warning: styles.error,
-              notValid: styles.notValid,
-            }}
-          />
-        </div>
-        <div className={styles.container}>
-          <span className={styles.label}>Display Name</span>
-          <FormInput
-            name='displayName'
-            type='text'
-            label='Display Name'
-            classes={{
-              container: styles.inputContainer,
-              input: styles.input,
-              warning: styles.error,
-              notValid: styles.notValid,
-            }}
-          />
-        </div>
-        <ImageUpload
-          name='file'
-          classes={{
-            uploadContainer: styles.imageUploadContainer,
-            inputContainer: styles.uploadInputContainer,
-            imgStyle: styles.imgStyle,
-          }}
-        />
-        <button type='submit' disabled={submitting}>
-          Submit
-        </button>
-      </Form>
+      {formikProps => (
+        <Form className={styles.updateContainer}>
+          {error && (
+            <Error
+              data={error.data}
+              status={error.status}
+              clearError={clearUserError}
+            />
+          )}
+          <div className={styles.container}>
+            <span className={styles.label}>First Name</span>
+            <FormInput
+              name='firstName'
+              type='text'
+              label='First Name'
+              classes={{
+                container: styles.inputContainer,
+                input: styles.input,
+                warning: styles.error,
+                notValid: styles.notValid,
+              }}
+            />
+          </div>
+          <div className={styles.container}>
+            <span className={styles.label}>Last Name</span>
+            <FormInput
+              name='lastName'
+              type='text'
+              label='LastName'
+              classes={{
+                container: styles.inputContainer,
+                input: styles.input,
+                warning: styles.error,
+                notValid: styles.notValid,
+              }}
+            />
+          </div>
+          <div className={styles.container}>
+            <span className={styles.label}>Display Name</span>
+            <FormInput
+              name='displayName'
+              type='text'
+              label='Display Name'
+              classes={{
+                container: styles.inputContainer,
+                input: styles.input,
+                warning: styles.error,
+                notValid: styles.notValid,
+              }}
+            />
+          </div>
+          <div className={styles.imageUploadContainer}>
+            <div className={styles.uploadInputContainer}>
+              <span>Support only images (*.png, *.gif, *.jpeg)</span>
+              <input
+                id='fileInput'
+                name='file'
+                type='file'
+                accept='.jpg, .png, .jpeg'
+                onChange={e => {
+                  setImageName(e.target.files[0].name);
+                  formikProps.setFieldValue('file', e.target.files[0]);
+                }}
+              />
+              <label htmlFor='fileInput'>Chose file</label>
+            </div>
+            <div className={styles.imgName}>{imageName}</div>
+          </div>
+          <button type='submit' disabled={submitting}>
+            Submit
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
@@ -90,6 +101,7 @@ const mapStateToProps = state => {
       firstName: data.firstName,
       lastName: data.lastName,
       displayName: data.displayName,
+      file: data.avatar,
     },
   };
 };
