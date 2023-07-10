f8b78162fb018d6b44d32b6b3c0f42fa46fc089b
alexcsych <alexsejsi4@gmail.com>
2023-07-10 15:32:52 +0300

    Add section-d

--- client/src/pages/HowItWorks/index.js
+++ client/src/pages/HowItWorks/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let navbarItems = document.getElementsByClassName('navbar-nav-item');
 
 Array.from(navbarItems).forEach(function (navbarItem) {
@@ -13,5 +15,40 @@ Array.from(navbarItems).forEach(function (navbarItem) {
     icon.style.color = '#4a4a4a';
     icon.classList.remove('fa-angle-up');
     icon.classList.add('fa-angle-down');
+  });
+});
+
+const buttons = document.querySelectorAll('.card-btn');
+
+buttons.forEach(function (button) {
+  const arrow = button.querySelector('i.arrow');
+  const content = button.closest('.cards').querySelector('.cards-content');
+
+  button.addEventListener('click', function () {
+    const activeButton = button
+      .closest('.section-d-pb-article')
+      .querySelector('.card-btn.active');
+    const activeContent = button
+      .closest('.section-d-pb-article')
+      .querySelector('.cards-content.active');
+
+    if (activeButton && activeButton !== button) {
+      activeButton.classList.remove('active');
+      activeButton.querySelector('i.arrow').classList.remove('fa-arrow-down');
+      activeButton.querySelector('i.arrow').classList.add('fa-arrow-right');
+      activeContent.classList.remove('active');
+    }
+
+    if (!button.classList.contains('active')) {
+      arrow.classList.remove('fa-arrow-right');
+      arrow.classList.add('fa-arrow-down');
+      content.classList.add('active');
+      button.classList.add('active');
+    } else {
+      arrow.classList.remove('fa-arrow-down');
+      arrow.classList.add('fa-arrow-right');
+      content.classList.remove('active');
+      button.classList.remove('active');
+    }
   });
 });
