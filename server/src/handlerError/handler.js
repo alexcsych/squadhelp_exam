03cc85a4ed4276637aacd476dde50fc12bbe4f17
alexcsych <alexsejsi4@gmail.com>
2023-07-15 10:58:07 +0300

    Add errorsLogger

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.resolve(__dirname, '..', '..', 'public/errorsLogger');
+
+if (!fs.existsSync(filePath)) {
+  fs.mkdirSync(filePath, {
+    recursive: true,
+  });
+}
+
 module.exports = (err, req, res, next) => {
+  const stackTrace = {};
   console.log(err);
   if (
     err.message ===
@@ -14,4 +25,22 @@ module.exports = (err, req, res, next) => {
   } else {
     res.status(err.code).send(err.message);
   }
+  const log = {
+    message: err.message || 'Server Error',
+    time: new Date().getTime(),
+    code: err.code || '500',
+    // stackTrace: { stackTrace },
+  };
+  const fileName = `${filePath}/errorsLogger.txt`;
+  let existingData = '';
+  if (fs.existsSync(fileName)) {
+    existingData = fs.readFileSync(fileName, 'utf8');
+  }
+  fs.writeFile(
+    fileName,
+    existingData + JSON.stringify(log) + '\n',
+    function (err) {
+      if (err) throw err;
+    }
+  );
 };
