c37407be97ffd4676d32fc7c74118c89eac64d30
alexcsych <alexsejsi4@gmail.com>
2023-07-18 12:08:59 +0300

    Add dateErrorsLogger and Add stacktrace-js

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const stacktrace = require('stacktrace-js');
 const path = require('path');
 const filePath = path.resolve(__dirname, '..', '..', 'public/errorsLogger');
 
@@ -8,39 +9,83 @@ if (!fs.existsSync(filePath)) {
   });
 }
 
-module.exports = (err, req, res, next) => {
-  const stackTrace = {};
-  console.log(err);
-  if (
-    err.message ===
-      'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
-    err.message ===
-      'new row for relation "Users" violates check constraint "Users_balance_ck"'
-  ) {
-    err.message = 'Not Enough money';
-    err.code = 406;
-  }
-  if (!err.message || !err.code) {
-    res.status(500).send('Server Error');
-  } else {
-    res.status(err.code).send(err.message);
-  }
-  const log = {
-    message: err.message || 'Server Error',
-    time: new Date().getTime(),
-    code: err.code || '500',
-    // stackTrace: { stackTrace },
-  };
-  const fileName = `${filePath}/errorsLogger.txt`;
-  let existingData = '';
+const fileName = `${filePath}/errorsLogger.txt`;
+let existingData = '';
+
+module.exports = async (err, req, res, next) => {
   if (fs.existsSync(fileName)) {
     existingData = fs.readFileSync(fileName, 'utf8');
   }
-  fs.writeFile(
-    fileName,
-    existingData + JSON.stringify(log) + '\n',
-    function (err) {
-      if (err) throw err;
+
+  try {
+    const stats = await fs.promises.stat(fileName);
+    const modificationDate = stats.mtime;
+    if (
+      modificationDate.getDate() < new Date().getDate() ||
+      modificationDate.getMonth() < new Date().getMonth() ||
+      modificationDate.getFullYear() < new Date().getFullYear()
+    ) {
+      const fileData = existingData
+        .split('\n')
+        .map(line => {
+          try {
+            return JSON.parse(line);
+          } catch (error) {
+            console.error('Error parsing JSON:', error);
+            return null;
+          }
+        })
+        .filter(data => data !== null);
+      const updatedData = fileData
+        .map(obj => {
+          const { message, code, time } = obj;
+          return { message, code, time };
+        })
+        .map(obj => JSON.stringify(obj))
+        .join('\n');
+      await fs.promises.writeFile(
+        `${filePath}/${modificationDate.getDate()}-${modificationDate.getMonth()}-${modificationDate.getFullYear()}.txt`,
+        updatedData
+      );
+      await fs.promises.writeFile(fileName, '');
     }
-  );
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+
+  const stackTracePromise = stacktrace.fromError(err);
+
+  stackTracePromise.then(stackframes => {
+    const stackTrace = stackframes.map(sf => sf.toString()).join(' ');
+    console.log(err);
+    if (
+      err.message ===
+        'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
+      err.message ===
+        'new row for relation "Users" violates check constraint "Users_balance_ck"'
+    ) {
+      err.message = 'Not Enough money';
+      err.code = 406;
+    }
+    if (!err.message || !err.code) {
+      res.status(500).send('Server Error');
+    } else {
+      res.status(err.code).send(err.message);
+    }
+    const log = {
+      message: err.message || 'Server Error',
+      time: new Date().getTime(),
+      code: err.code || 500,
+      stackTrace: stackTrace,
+    };
+    if (fs.existsSync(fileName)) {
+      existingData = fs.readFileSync(fileName, 'utf8');
+    }
+    fs.promises
+      .writeFile(fileName, existingData + JSON.stringify(log) + '\n')
+      .catch(error => {
+        console.error('Error writing file:', error);
+      });
+  });
 };
