eb57679dbe8a18fbfa9d85f0535c8dea74bd774e
alexcsych <alexsejsi4@gmail.com>
2023-08-07 11:24:36 +0300

    Add eventStore

---
+++ client/src/store/slices/eventSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
+const eventSlice = createSlice({
+  name: 'events',
+  initialState: { events: [] },
+  reducers: {
+    createEvent (state, action) {
+      state.events.push({
+        ...action.payload,
+        createdAt: new Date().toISOString(),
+        progress: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        id: uuidv4(),
+      });
+    },
+    deleteEvent (state, action) {
+      state.events = state.events.filter(e => e.id !== action.payload);
+    },
+    updateEventProgress (state, action) {
+      const updatedEvents = action.payload;
+      state.events = updatedEvents;
+    },
+  },
+});
+
+const { reducer, actions } = eventSlice;
+
+export const { createEvent, deleteEvent, updateEvent, updateEventProgress } =
+  actions;
+
+export default reducer;
