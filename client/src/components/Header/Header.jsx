95d8fdb6bd3e05356c05d670e4dfa930b55e10fd
alexcsych <alexsejsi4@gmail.com>
2023-08-24 15:47:44 +0300

    Fix start contests btn

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -269,7 +269,7 @@ class Header extends React.Component {
                 </li>
               </ul>
             </div>
-            {this.props.data && this.props.data.role !== CONSTANTS.CREATOR && (
+            {this.props.data && this.props.data.role == CONSTANTS.CUSTOMER && (
               <div
                 className={styles.startContestBtn}
                 onClick={this.startContests}
