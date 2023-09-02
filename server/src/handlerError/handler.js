7d5fe9dabbec354c28fb4d000e5ad834401073e8
alexcsych <alexsejsi4@gmail.com>
2023-09-02 16:17:06 +0300

    Improve handlerError

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -10,14 +10,24 @@ if (!fs.existsSync(filePath)) {
 }
 
 const fileName = `${filePath}/errorsLogger.txt`;
-let existingData = '';
 
-module.exports = async (err, req, res, next) => {
-  if (fs.existsSync(fileName)) {
-    existingData = fs.readFileSync(fileName, 'utf8');
-  }
-
+const logError = async err => {
   try {
+    let existingData;
+    let logData;
+
+    if (fs.existsSync(fileName)) {
+      existingData = fs.readFileSync(fileName, 'utf8');
+      if (existingData.trim() !== '') {
+        try {
+          logData = JSON.parse(existingData);
+        } catch (error) {
+          console.error('Error:', error);
+          logData = [];
+        }
+      }
+    }
+
     const stats = await fs.promises.stat(fileName);
     const modificationDate = stats.mtime;
     if (
@@ -25,72 +35,55 @@ module.exports = async (err, req, res, next) => {
       modificationDate.getMonth() < new Date().getMonth() ||
       modificationDate.getFullYear() < new Date().getFullYear()
     ) {
-      const fileData = existingData
-        .split('\n')
-        .map(line => {
-          if (line.trim() === '') {
-            return null;
-          }
-          try {
-            return JSON.parse(line);
-          } catch (error) {
-            console.error('Error parsing JSON:', error);
-            return null;
-          }
-        })
-        .filter(data => data !== null);
-      const updatedData = fileData
-        .map(obj => {
-          const { message, code, time } = obj;
-          return { message, code, time };
-        })
-        .map(obj => JSON.stringify(obj))
-        .join('\n');
+      const updatedData = logData.map(obj => {
+        const { message, code, time } = obj;
+        return { message, code, time };
+      });
+      const allLogs = JSON.stringify(updatedData);
       await fs.promises.writeFile(
         `${filePath}/${modificationDate.getFullYear()}-${
           modificationDate.getMonth() + 1
         }-${modificationDate.getDate()}.txt`,
-        updatedData
+        allLogs
       );
-      await fs.promises.writeFile(fileName, '');
+      logData = [];
     }
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
-  }
 
-  const stackTracePromise = stacktrace.fromError(err);
-
-  stackTracePromise.then(stackframes => {
+    const stackframes = await stacktrace.fromError(err);
     const stackTrace = stackframes.map(sf => sf.toString()).join(' ');
-    console.log(err);
-    if (
-      err.message ===
-        'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
-      err.message ===
-        'new row for relation "Users" violates check constraint "Users_balance_ck"'
-    ) {
-      err.message = 'Not Enough money';
-      err.code = 406;
-    }
-    if (!err.message || !err.code) {
-      res.status(500).send('Server Error');
-    } else {
-      res.status(err.code).send(err.message);
-    }
+
     const log = {
       message: err.message || 'Server Error',
       time: new Date().getTime(),
       code: err.code || 500,
       stackTrace: stackTrace,
     };
-    if (fs.existsSync(fileName)) {
-      existingData = fs.readFileSync(fileName, 'utf8');
-    }
-    fs.promises
-      .writeFile(fileName, existingData + JSON.stringify(log) + '\n')
-      .catch(error => {
-        console.error('Error writing file:', error);
-      });
-  });
+
+    logData.push(log);
+    const allLogs = JSON.stringify(logData);
+    fs.promises.writeFile(fileName, allLogs).catch(error => {
+      console.error('Error writing file:', error);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+module.exports = async (err, req, res, next) => {
+  if (
+    err.message ===
+      'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
+    err.message ===
+      'new row for relation "Users" violates check constraint "Users_balance_ck"'
+  ) {
+    err.message = 'Not Enough money';
+    err.code = 406;
+  }
+  if (!err.message || !err.code) {
+    res.status(500).send('Server Error');
+  } else {
+    res.status(err.code).send(err.message);
+  }
+
+  await logError(err);
 };
