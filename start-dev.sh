2cec90f426a3c9a6fcd81a411bed58e32f64d7a8
alexcsych <alexsejsi4@gmail.com>
2023-10-16 18:52:44 +0300

    Fix start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -8,17 +8,17 @@
 started_at=$(date +"%s")
 
 echo "-----> Provisioning containers"
-docker-compose --file docker-compose-dev.yaml up
+docker compose --file docker-compose-dev.yaml up
 echo ""
 
 # Run Sequalize's migrations.
 echo "-----> Running application migrations"
-docker exec -it squadhelp_server-dev_1 sequelize db:migrate
+docker exec -it squadhelp_exam-server-dev_1 sequelize db:migrate
 echo ""
 
 # Run Sequalize's seeds.
 echo "-----> Running application seeds"
-docker exec -it squadhelp_server-dev_1 sequelize db:seed:all
+docker exec -it squadhelp_exam-server-dev_1 sequelize db:seed:all
 echo "<----- Seeds created"
 
 ended_at=$(date +"%s")
